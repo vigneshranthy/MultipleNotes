4c3e94e13d1b2a01149655c13eaa301babc6a871
vigneshranthy <vignesh@ranthy.com>
2015-10-12 17:00:58 +0530

    Update NoteList.java

--- NoteList.java
+++ NoteList.java
@@ -47,7 +47,7 @@ public class NoteList extends ListActivity {
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+	
 		getMenuInflater().inflate(R.menu.notelist_menu, menu);
 		return true;		
 	}
